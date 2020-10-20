de9c1641d77782dcb87b3c12d5059b9fcdbd613f
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-10-20 15:44:18 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,16 @@
 require 'pry'
 
 def find_element_index(array, value_to_find)
+<<<<<<< HEAD
   target_index = nil
+=======
+  
+>>>>>>> dccd5225fcb4843cc1a1e3549f6090d75e522545
   counter = 0
   array.each do |element|
     if element == value_to_find
       
+<<<<<<< HEAD
       target_index = counter
     else
       counter += 1
@@ -14,4 +19,16 @@ def find_element_index(array, value_to_find)
   end
  
 target_index
+=======
+      target_index = counter.to_s
+    else
+      counter += 1
+    end
+  end
+  if target_index == true
+    return target_index
+  else
+    nil
+end
+>>>>>>> dccd5225fcb4843cc1a1e3549f6090d75e522545
 end
